096f3c41e0564e2fff906a973cd1bfe19f658432
MariamKochadze <kochadzemariam80@gmail.com>
2024-12-30 17:56:01 +0400

    fix: modify file

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+    console.log(`Request: ${req.method} ${req.url}`);
+    next();
+});
+
 server.use(router);
+
+// Global error handler
+server.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 server.listen(3000, () => {
     console.log('JSON Server is running');
