373a5ce6c8fe7b4b4f7c74a74298c95d1ecc3bad
MariamKochadze <kochadzemariam80@gmail.com>
2024-12-30 18:06:49 +0400

    feat: modify json

--- index.js
+++ index.js
@@ -4,17 +4,10 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-        return res.status(200).json({});
-    }
-    next();
-});
-
 server.use(router);
+
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+});
 
 module.exports = server;
