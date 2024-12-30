81ad7a2148bffc94852d87bb42dee3fcf7bf0d1e
MariamKochadze <kochadzemariam80@gmail.com>
2024-12-30 17:48:34 +0400

    fix: json file

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    next();
-});
-
 server.use(router);
+
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+});
 
 module.exports = server;
